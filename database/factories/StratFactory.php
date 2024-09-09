dfe8a8f1ba0f2fcb9cd09468493a5321b6a3f78c
Nick Chaloult <npchaloult@gmail.com>
2024-09-09 10:12:41 -0400

    Temporarily remove logic to add a value for num_images

--- database/factories/StratFactory.php
+++ database/factories/StratFactory.php
@@ -17,12 +17,17 @@ class StratFactory extends Factory
      */
     public function definition(): array
     {
+        // TODO: Write logic to add num_images sometimes.
+        // Faker's optional() method isn't good enough. If that optional() call
+        // decides we shouldn't have num_images for a particular new strat, it
+        // returns null --- not allowing the default value of 0 to take over.
+        // When we try to write this to the database, this throws a "not null
+        // violation".
         return [
             'user_id' => User::factory(),
             'title' => fake()->sentence(),
             'attacker_side_notes' => fake()->optional()->paragraphs(5, true),
             'defender_side_notes' => fake()->optional()->paragraphs(5, true),
-            'num_images' => fake()->optional()->numberBetween(0, 6),
         ];
     }
 }
