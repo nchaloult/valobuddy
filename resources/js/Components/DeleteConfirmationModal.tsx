2793b73c5acda88e0295c91190a70ba8f2045028
Nick Chaloult <npchaloult@gmail.com>
2024-09-10 10:55:38 -0400

    Animate the hazard tape graphic on the DeleteConfirmationModal

--- resources/js/Components/DeleteConfirmationModal.tsx
+++ resources/js/Components/DeleteConfirmationModal.tsx
@@ -1,12 +1,12 @@
 // TODO: Type props.
 export default function DeleteConfirmationModal({ stratId, stratTitle }) {
   return (
-    <div className="relative z-50" style={{ animation: "fadeIn 2s" }}>
+    <div className="relative z-50" style={{ animation: "fade-in 0.2s" }}>
       {/* Background overlay */}
       {/* <div className="fixed inset-0 bg-black bg-opacity-50 backdrop-blur-sm transition-opacity"></div> */}
-      <div className="fixed inset-0 bg-gradient-to-b from-red-500 to-transparent from-[-40%] to-60%"></div>
+      <div className="fixed inset-0 bg-gradient-to-b from-red-500 to-transparent from-[-60%] to-80%">&nbsp;</div>
 
-      <svg xmlns="http://www.w3.org/2000/svg" className="fixed z-10 w-svw h-svh opacity-10">
+      <svg xmlns="http://www.w3.org/2000/svg" className="fixed z-10 w-svw h-svh opacity-20">
         <defs>
           <pattern id="plusPattern" x="0" y="0" width={80} height={80} patternUnits="userSpaceOnUse">
             <path
@@ -22,8 +22,19 @@ export default function DeleteConfirmationModal({ stratId, stratTitle }) {
       <div className="fixed inset-0 z-50 w-screen overflow-y-auto">
         <div className="flex items-center justify-center min-h-full">
           <div className="p-4 w-2/3 lg:w-1/2 bg-neutral-800">
+
             {/* "Hazard tape" graphic. */}
-            <div className="mb-2 h-4" style={{ backgroundImage: "repeating-linear-gradient(-45deg, rgba(0,0,0,0), rgba(0,0,0,0) 0.5rem, #404040 0.5rem, #404040 1rem )" }}>&nbsp;</div>
+            <div className="mb-2 h-4 overflow-hidden">
+              <div
+                className="w-svw"
+                style={{
+                  backgroundImage: "repeating-linear-gradient(-45deg, rgba(0,0,0,0), rgba(0,0,0,0) 0.5rem, #404040 0.5rem, #404040 1rem )",
+                  animation: "hazard-tape-marquee-ltor 1s linear infinite",
+                }}
+              >
+                &nbsp;
+              </div>
+            </div>
 
             {/* TODO: What tag should we use for this? */}
             <h4 className="text-3xl font-['Druk_Wide_Bold']">DELETE STRAT</h4>
