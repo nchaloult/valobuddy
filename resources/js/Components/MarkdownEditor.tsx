83cded083f431106133393fb3757d0e2bbfb5421
Nick Chaloult <npchaloult@gmail.com>
2024-09-12 14:49:31 -0400

    Add many more buttons in the MarkdownEditor's menu bar

--- resources/js/Components/MarkdownEditor.tsx
+++ resources/js/Components/MarkdownEditor.tsx
@@ -1,6 +1,7 @@
 import { EditorProvider, useCurrentEditor } from "@tiptap/react";
 import StarterKit from "@tiptap/starter-kit";
 import ImageExtension from "@tiptap/extension-image";
+import LinkExtension from "@tiptap/extension-link";
 import { Dispatch, SetStateAction } from "react";
 function MenuBar() {
   const { editor } = useCurrentEditor();
@@ -8,10 +9,24 @@ function MenuBar() {
     return null;
   }
 
+  function promptForHyperlink() {
+    if (editor?.isActive("link")) {
+      return editor.chain().focus().unsetLink().run();
+    }
+
+    const href = prompt("URL to link to:");
+    if (!href) {
+      return editor?.chain().focus().run();
+    }
+
+    return editor?.chain().focus().setLink({ href }).run();
+  }
+
   return (
     <div className="bg-neutral-600 pb-0.5">
-      <div className="grid gap-0.5 grid-flow-col auto-cols-max [&>*]:px-2 [&>*]:py-1 [&>*]:text-sm [&>*]:font-['Space_Mono']">
+      <div className="grid gap-0.5 grid-flow-col auto-cols-max [&>*]:px-2.5 [&>*]:py-1 [&>*]:text-sm [&>*]:font-['Space_Mono']">
         <button
+          title="Bold"
           onClick={() => editor.chain().focus().toggleBold().run()}
           disabled={!editor.can().chain().focus().toggleBold().run()}
           className={`bg-gradient-to-r from-red-600 to-neutral-900 from-50% to-50% bg-right-bottom bg-[length:201%_100%] outline-none hover:bg-left-bottom hover:text-neutral-900 focus:bg-left-bottom focus:text-neutral-900 transition-all duration-200 ${
@@ -19,6 +34,151 @@ function MenuBar() {
           }`}
         >
           B
+        </button>
+        <button
+          title="Italic"
+          onClick={() => editor.chain().focus().toggleItalic().run()}
+          disabled={!editor.can().chain().focus().toggleItalic().run()}
+          className={`bg-gradient-to-r from-red-600 to-neutral-900 from-50% to-50% bg-right-bottom bg-[length:201%_100%] outline-none hover:bg-left-bottom hover:text-neutral-900 focus:bg-left-bottom focus:text-neutral-900 transition-all duration-200 ${
+            editor.isActive("italic") ? "bg-left-bottom" : ""
+          }`}
+        >
+          I
+        </button>
+        <button
+          title="Strikethrough"
+          onClick={() => editor.chain().focus().toggleStrike().run()}
+          disabled={!editor.can().chain().focus().toggleStrike().run()}
+          className={`bg-gradient-to-r from-red-600 to-neutral-900 from-50% to-50% bg-right-bottom bg-[length:201%_100%] outline-none hover:bg-left-bottom hover:text-neutral-900 focus:bg-left-bottom focus:text-neutral-900 transition-all duration-200 ${
+            editor.isActive("strike") ? "bg-left-bottom" : ""
+          }`}
+        >
+          S
+        </button>
+        <button
+          title="Block quote"
+          onClick={() => editor.chain().focus().toggleBlockquote().run()}
+          disabled={!editor.can().chain().focus().toggleBlockquote().run()}
+          className={`bg-gradient-to-r from-red-600 to-neutral-900 from-50% to-50% bg-right-bottom bg-[length:201%_100%] outline-none hover:bg-left-bottom hover:text-neutral-900 focus:bg-left-bottom focus:text-neutral-900 transition-all duration-200 ${
+            editor.isActive("blockquote") ? "bg-left-bottom" : ""
+          }`}
+        >
+          "
+        </button>
+        <button
+          title="Bulleted list"
+          onClick={() => editor.chain().focus().toggleBulletList().run()}
+          disabled={!editor.can().chain().focus().toggleBulletList().run()}
+          className={`bg-gradient-to-r from-red-600 to-neutral-900 from-50% to-50% bg-right-bottom bg-[length:201%_100%] outline-none hover:bg-left-bottom hover:text-neutral-900 focus:bg-left-bottom focus:text-neutral-900 transition-all duration-200 ${
+            editor.isActive("bulletList") ? "bg-left-bottom" : ""
+          }`}
+        >
+          *
+        </button>
+        <button
+          title="Numbered list"
+          onClick={() => editor.chain().focus().toggleOrderedList().run()}
+          disabled={!editor.can().chain().focus().toggleOrderedList().run()}
+          className={`bg-gradient-to-r from-red-600 to-neutral-900 from-50% to-50% bg-right-bottom bg-[length:201%_100%] outline-none hover:bg-left-bottom hover:text-neutral-900 focus:bg-left-bottom focus:text-neutral-900 transition-all duration-200 ${
+            editor.isActive("orderedList") ? "bg-left-bottom" : ""
+          }`}
+        >
+          1
+        </button>
+        {/* No disabled state on this button. Honestly, we can probably get rid
+            of the disabled states on all buttons. */}
+        <button
+          title="Hyperlink"
+          onClick={promptForHyperlink}
+          className={`bg-gradient-to-r from-red-600 to-neutral-900 from-50% to-50% bg-right-bottom bg-[length:201%_100%] outline-none hover:bg-left-bottom hover:text-neutral-900 focus:bg-left-bottom focus:text-neutral-900 transition-all duration-200 ${
+            editor.isActive("link") ? "bg-left-bottom" : ""
+          }`}
+        >
+          L
+        </button>
+        <button
+          title="Heading 1"
+          onClick={() =>
+            editor.chain().focus().toggleHeading({ level: 1 }).run()
+          }
+          disabled={
+            !editor.can().chain().focus().toggleHeading({ level: 1 }).run()
+          }
+          className={`bg-gradient-to-r from-red-600 to-neutral-900 from-50% to-50% bg-right-bottom bg-[length:201%_100%] outline-none hover:bg-left-bottom hover:text-neutral-900 focus:bg-left-bottom focus:text-neutral-900 transition-all duration-200 ${
+            editor.isActive("heading", { level: 1 }) ? "bg-left-bottom" : ""
+          }`}
+        >
+          H1
+        </button>
+        <button
+          title="Heading 2"
+          onClick={() =>
+            editor.chain().focus().toggleHeading({ level: 2 }).run()
+          }
+          disabled={
+            !editor.can().chain().focus().toggleHeading({ level: 2 }).run()
+          }
+          className={`bg-gradient-to-r from-red-600 to-neutral-900 from-50% to-50% bg-right-bottom bg-[length:201%_100%] outline-none hover:bg-left-bottom hover:text-neutral-900 focus:bg-left-bottom focus:text-neutral-900 transition-all duration-200 ${
+            editor.isActive("heading", { level: 2 }) ? "bg-left-bottom" : ""
+          }`}
+        >
+          H2
+        </button>
+        <button
+          title="Heading 3"
+          onClick={() =>
+            editor.chain().focus().toggleHeading({ level: 3 }).run()
+          }
+          disabled={
+            !editor.can().chain().focus().toggleHeading({ level: 3 }).run()
+          }
+          className={`bg-gradient-to-r from-red-600 to-neutral-900 from-50% to-50% bg-right-bottom bg-[length:201%_100%] outline-none hover:bg-left-bottom hover:text-neutral-900 focus:bg-left-bottom focus:text-neutral-900 transition-all duration-200 ${
+            editor.isActive("heading", { level: 3 }) ? "bg-left-bottom" : ""
+          }`}
+        >
+          H3
+        </button>
+        <button
+          title="Heading 4"
+          onClick={() =>
+            editor.chain().focus().toggleHeading({ level: 4 }).run()
+          }
+          disabled={
+            !editor.can().chain().focus().toggleHeading({ level: 4 }).run()
+          }
+          className={`bg-gradient-to-r from-red-600 to-neutral-900 from-50% to-50% bg-right-bottom bg-[length:201%_100%] outline-none hover:bg-left-bottom hover:text-neutral-900 focus:bg-left-bottom focus:text-neutral-900 transition-all duration-200 ${
+            editor.isActive("heading", { level: 4 }) ? "bg-left-bottom" : ""
+          }`}
+        >
+          H4
+        </button>
+        <button
+          title="Heading 5"
+          onClick={() =>
+            editor.chain().focus().toggleHeading({ level: 5 }).run()
+          }
+          disabled={
+            !editor.can().chain().focus().toggleHeading({ level: 5 }).run()
+          }
+          className={`bg-gradient-to-r from-red-600 to-neutral-900 from-50% to-50% bg-right-bottom bg-[length:201%_100%] outline-none hover:bg-left-bottom hover:text-neutral-900 focus:bg-left-bottom focus:text-neutral-900 transition-all duration-200 ${
+            editor.isActive("heading", { level: 5 }) ? "bg-left-bottom" : ""
+          }`}
+        >
+          H5
+        </button>
+        <button
+          title="Heading 6"
+          onClick={() =>
+            editor.chain().focus().toggleHeading({ level: 6 }).run()
+          }
+          disabled={
+            !editor.can().chain().focus().toggleHeading({ level: 6 }).run()
+          }
+          className={`bg-gradient-to-r from-red-600 to-neutral-900 from-50% to-50% bg-right-bottom bg-[length:201%_100%] outline-none hover:bg-left-bottom hover:text-neutral-900 focus:bg-left-bottom focus:text-neutral-900 transition-all duration-200 ${
+            editor.isActive("heading", { level: 6 }) ? "bg-left-bottom" : ""
+          }`}
+        >
+          H6
         </button>
       </div>
     </div>
@@ -47,7 +207,7 @@ export default function MarkdownEditor({
     <div className="bg-neutral-700 border-2 border-neutral-600 hover:border-neutral-500 has-[:focus]:border-neutral-400 transition-all duration-200">
       <EditorProvider
         slotBefore={<MenuBar />}
-        extensions={[StarterKit, ImageExtension]}
+        extensions={[StarterKit, ImageExtension, LinkExtension]}
         editorProps={{
           attributes: {
             spellcheck: "false", // Performance enhancement, plus we don't really need it.
