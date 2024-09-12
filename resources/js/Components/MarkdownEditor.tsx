e5d54ffd57b097c83484f949de57a5577529b4a1
Nick Chaloult <npchaloult@gmail.com>
2024-09-12 13:52:11 -0400

    Remove unnecessary file size, file type, and image dimension checks
    Imgur will bear the load of hosting images, so we don't care about
    this stuff.

--- resources/js/Components/MarkdownEditor.tsx
+++ resources/js/Components/MarkdownEditor.tsx
@@ -2,7 +2,6 @@ import { EditorProvider, useCurrentEditor } from "@tiptap/react";
 import StarterKit from "@tiptap/starter-kit";
 import ImageExtension from "@tiptap/extension-image";
 import { Dispatch, SetStateAction } from "react";
-
 function MenuBar() {
   const { editor } = useCurrentEditor();
   if (!editor) {
@@ -71,64 +70,49 @@ export default function MarkdownEditor({
             }
 
             let file = event.dataTransfer.files[0]; // The dropped file.
-            let filesize = (file.size / 1024 / 1024).toFixed(4); // The filesize in MB.
-            if (
-              !(file.type === "image/jpeg" || file.type === "image/png") ||
-              filesize >= 10
-            ) {
-              window.alert(
-                "Images need to be in jpg or png format and less than 10mb in size."
-              );
-              return false;
-            }
+
             // Check the image's dimensions.
             let _URL = window.URL || window.webkitURL;
             let img = new Image();
             img.src = _URL.createObjectURL(file);
             img.onload = function () {
-              if (this.width > 5000 || this.height > 5000) {
-                window.alert(
-                  "Your images need to be less than 5000 pixels in height and width."
-                );
-              } else {
-                uploadImage(file)
-                  .then((publicUrl) => {
-                    // Pre-load the image before responding so loading indicators
-                    // can stay and swaps out smoothly when the image is ready.
-                    let image = new Image();
-                    image.src = publicUrl;
-                    image.onload = function () {
-                      // Place the now uploaded image in the editor where it was
-                      // dropped.
+              uploadImage(file)
+                .then((publicUrl) => {
+                  // Pre-load the image before responding so loading indicators
+                  // can stay and swaps out smoothly when the image is ready.
+                  let image = new Image();
+                  image.src = publicUrl;
+                  image.onload = function () {
+                    // Place the now uploaded image in the editor where it was
+                    // dropped.
 
-                      const { schema } = view.state;
-                      const coordinates = view.posAtCoords({
-                        left: event.clientX,
-                        top: event.clientY,
-                      });
+                    const { schema } = view.state;
+                    const coordinates = view.posAtCoords({
+                      left: event.clientX,
+                      top: event.clientY,
+                    });
 
-                      // Create the TipTap Image element/"node".
-                      const node = schema.nodes.image.create({
-                        src: publicUrl,
-                      });
+                    // Create the TipTap Image element/"node".
+                    const node = schema.nodes.image.create({
+                      src: publicUrl,
+                    });
 
-                      // Place that "node" in the correct position.
-                      const transaction = view.state.tr.insert(
-                        coordinates.pos,
-                        node
-                      );
+                    // Place that "node" in the correct position.
+                    const transaction = view.state.tr.insert(
+                      coordinates.pos,
+                      node
+                    );
 
-                      return view.dispatch(transaction);
-                    };
-                  })
-                  .catch(function (error) {
-                    if (error) {
-                      window.alert(
-                        "There was a problem uploading your image, please try again."
-                      );
-                    }
-                  });
-              }
+                    return view.dispatch(transaction);
+                  };
+                })
+                .catch(function (error) {
+                  if (error) {
+                    window.alert(
+                      "There was a problem uploading your image, please try again."
+                    );
+                  }
+                });
             };
 
             return true;
