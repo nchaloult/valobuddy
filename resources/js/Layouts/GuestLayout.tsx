acf9e124b4c313f44e81c02d85eca360a1eb678e
Nick Chaloult <npchaloult@gmail.com>
2024-09-17 17:15:15 -0400

    Make GuestLayout generic, prepping it for use across the site

--- resources/js/Layouts/GuestLayout.tsx
+++ resources/js/Layouts/GuestLayout.tsx
@@ -1,19 +1,17 @@
-import ApplicationLogo from '@/Components/ApplicationLogo';
-import { Link } from '@inertiajs/react';
-import { PropsWithChildren } from 'react';
+import Header from "@/Components/Header";
+import { PropsWithChildren } from "react";
 
-export default function Guest({ children }: PropsWithChildren) {
-    return (
-        <div className="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-gray-100">
-            <div>
-                <Link href="/">
-                    <ApplicationLogo className="w-20 h-20 fill-current text-gray-500" />
-                </Link>
-            </div>
+export default function GuestLayout({ children }: PropsWithChildren) {
+  return (
+    // TODO: Why is there a bit of vertical scroll when the page doesn't stretch
+    // beyond the screen height? Padding or margin somewhere? Why isn't the grow
+    // class working like I think it should?
+    <div className="flex flex-col min-h-svh">
+      <Header />
 
-            <div className="w-full sm:max-w-md mt-6 px-6 py-4 bg-white shadow-md overflow-hidden sm:rounded-lg">
-                {children}
-            </div>
-        </div>
-    );
+      <div className="grow">{children}</div>
+
+      {/* TODO: Add Footer component. */}
+    </div>
+  );
 }
