264ad928f401c0fec27957fb5a44ad6f397af064
Nick Chaloult <npchaloult@gmail.com>
2024-09-26 23:56:49 -0400

    Polish up disabled styles

--- resources/js/Components/WidePrimaryButton.tsx
+++ resources/js/Components/WidePrimaryButton.tsx
@@ -16,8 +16,10 @@ export default function WidePrimaryButton({
       className={
         `${
           fullWidth ? "w-full" : "w-1/3 min-w-48"
-        } px-4 py-3 text-center text-sm font-['Space_Mono'] border-2 border-neutral-700 bg-gradient-to-t from-red-600 to-neutral-900 from-50% to-50% bg-top bg-[length:100%_200%] outline-none hover:bg-bottom hover:text-neutral-900 hover:border-red-500 focus:bg-bottom focus:text-neutral-900 focus:border-red-500 transition-all duration-200 ${
-          disabled && "opacity-25"
+        } px-4 py-3 text-center text-sm font-['Space_Mono'] border-2 bg-gradient-to-t from-red-600 to-neutral-900 from-50% to-50% bg-top bg-[length:100%_200%] outline-none transition-all duration-200 ${
+          disabled
+            ? "border-neutral-900 cursor-default"
+            : "border-neutral-700 hover:bg-bottom hover:text-neutral-900 hover:border-red-500 focus:bg-bottom focus:text-neutral-900 focus:border-red-500"
         } ` + className
       }
       disabled={disabled}
