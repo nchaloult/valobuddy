847b8ca16731119108caeace923cb22d51974626
Nick Chaloult <npchaloult@gmail.com>
2024-09-29 15:39:25 -0400

    Add type hints for return values on Eloquent relationship functions

--- app/Models/Strat.php
+++ app/Models/Strat.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Strat extends Model
 {
@@ -15,7 +16,7 @@ class Strat extends Model
         'num_images' => 0,
     ];
 
-    public function user()
+    public function user(): BelongsTo
     {
         return $this->belongsTo(User::class);
     }
