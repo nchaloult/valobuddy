d186f2f37209a4d14d146b753c1ebbd7175c32f8
Nick Chaloult <npchaloult@gmail.com>
2024-10-02 16:28:37 -0400

    Add the MapSelector to the Collection page

--- resources/js/Pages/Collection.tsx
+++ resources/js/Pages/Collection.tsx
@@ -1,14 +1,15 @@
 import { PageProps } from "@/types";
 import Header from "@/Components/Header";
+import MapSelector from "@/Components/MapSelector";
 
 export default function Collection({ auth }: PageProps) {
   return (
-    <>
+    <div className="flex flex-col h-svh">
       <Header user={auth.user} />
 
-      <main>
-        <p>Collection. Map selector goes here.</p>
+      <main className="grow justify-center items-center p-6 lg:p-16">
+        <MapSelector />
       </main>
-    </>
+    </div>
   );
 }
