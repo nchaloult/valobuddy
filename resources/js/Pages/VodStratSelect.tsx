f7bc62628729dffe1e4be0ae22bfff36464c7918
Nick Chaloult <npchaloult@gmail.com>
2024-10-03 22:32:43 -0400

    Add VodStratSelect page

---
+++ resources/js/Pages/VodStratSelect.tsx
@@ -0,0 +1,36 @@
+import { PageProps } from "@/types";
+import Header from "@/Components/Header";
+import { Link } from "@inertiajs/react";
+
+type Props = {
+  map: string;
+  agent: string;
+};
+export default function AgentSelect({ auth, map, agent }: PageProps & Props) {
+  return (
+    <div className="flex flex-col h-svh">
+      <Header user={auth.user} />
+
+      <main className="flex grow text-6xl font-['Druk_Wide_Bold']">
+        <div className="w-full grid grid-cols-2">
+          <Link
+            href={route("vods.index", { map, agent })}
+            className="group flex justify-center items-center hover:bg-green-400 transition"
+          >
+            <span className="bg-gradient-to-b from-neutral-300 to-white text-transparent bg-clip-text group-hover:text-white transition">
+              VODS
+            </span>
+          </Link>
+          <Link
+            href={route("strats.index", { map, agent })}
+            className="group flex justify-center items-center hover:bg-red-500 transition"
+          >
+            <span className="bg-gradient-to-b from-neutral-300 to-white text-transparent bg-clip-text group-hover:text-white transition">
+              STRATS
+            </span>
+          </Link>
+        </div>
+      </main>
+    </div>
+  );
+}
