7f2de4177c664e8823e502cd3bd1b4970f1cd106
Nick Chaloult <npchaloult@gmail.com>
2024-10-04 10:23:41 -0400

    Add Agent enum

---
+++ app/Enums/Agent.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Enums;
+
+enum Agent: string
+{
+    case ASTRA = 'astra';
+    case BREACH = 'breach';
+    case BRIMSTONE = 'brimstone';
+    case CHAMBER = 'chamber';
+    case CLOVE = 'clove';
+    case CYPHER = 'cypher';
+    case DEADLOCK = 'deadlock';
+    case FADE = 'fade';
+    case GEKKO = 'gekko';
+    case HARBOR = 'harbor';
+    case ISO = 'iso';
+    case JETT = 'jett';
+    case KAYO = 'kayo';
+    case KILLJOY = 'killjoy';
+    case NEON = 'neon';
+    case OMEN = 'omen';
+    case PHOENIX = 'phoenix';
+    case RAZE = 'raze';
+    case REYNA = 'reyna';
+    case SAGE = 'sage';
+    case SKYE = 'skye';
+    case SOVA = 'sova';
+    case VIPER = 'viper';
+    case YORU = 'yoru';
+}
