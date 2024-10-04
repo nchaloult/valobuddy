867c8db4fff0d65cdb32c6e0e81687910451f8a8
Nick Chaloult <npchaloult@gmail.com>
2024-10-04 10:20:10 -0400

    Add Map enum

---
+++ app/Enums/Map.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Enums;
+
+enum Map: string
+{
+    case ABYSS = 'abyss';
+    case ASCENT = 'ascent';
+    case BIND = 'bind';
+    case BREEZE = 'breeze';
+    case FRACTURE = 'fracture';
+    case HAVEN = 'haven';
+    case ICEBOX = 'icebox';
+    case LOTUS = 'lotus';
+    case PEARL = 'pearl';
+    case SPLIT = 'split';
+    case SUNSET = 'sunset';
+}
